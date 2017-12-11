39c01a3c4b6c34084cdf3c3e520e451754da0859
Marco <marcocolusso99@gmail.com>
2017-12-11 13:01:32 +0100

    Prova Meteo

--- IPMeteoLocation/src/controller/ControllerMeteoWindow.java
+++ IPMeteoLocation/src/controller/ControllerMeteoWindow.java
@@ -2,6 +2,11 @@ package controller;
 
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.net.URL;
+
+import javax.xml.bind.JAXBContext;
+import javax.xml.bind.JAXBException;
+import javax.xml.bind.Unmarshaller;
 
 import model.Weather;
 import view.MeteoWindow;
@@ -10,6 +15,18 @@ public class ControllerMeteoWindow implements ActionListener {
 	
 	private Weather model;
 	private MeteoWindow view;
+	
+	public ControllerMeteoWindow() {
+		URL file = Weather.class.getResource("weatherData.xml");
+		Weather customer;
+		try {
+			JAXBContext jaxbContext =JAXBContext.newInstance(Weather.class);
+			Unmarshaller jaxbUnmarshaller = jaxbContext.createUnmarshaller();
+			customer = (Weather) jaxbUnmarshaller.unmarshal(file);
+		} catch (JAXBException e) {
+			e.printStackTrace();
+		}
+	}
 
 	public void actionPerformed(ActionEvent e) {
 		
--- IPMeteoLocation/src/main/Main.java
+++ IPMeteoLocation/src/main/Main.java
@@ -1,5 +1,12 @@
 package main;
 
+import java.net.URL;
+
+import javax.xml.bind.JAXBContext;
+import javax.xml.bind.JAXBException;
+import javax.xml.bind.Unmarshaller;
+
+import model.Weather;
 import view.MeteoWindow;
 
 public class Main {
@@ -7,6 +14,15 @@ public class Main {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		new MeteoWindow();
+		URL file = Weather.class.getResource("weatherData.xml");
+		Weather customer;
+		try {
+			JAXBContext jaxbContext =JAXBContext.newInstance(Weather.class);
+			Unmarshaller jaxbUnmarshaller = jaxbContext.createUnmarshaller();
+			customer = (Weather) jaxbUnmarshaller.unmarshal(file);
+		} catch (JAXBException e) {
+			e.printStackTrace();
+		}
 	}
 
 }
