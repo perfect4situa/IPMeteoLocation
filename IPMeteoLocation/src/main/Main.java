8ea5b4cac7b7b9cbc0f51e9781ac810da7915c74
Marco <marcocolusso99@gmail.com>
2017-12-12 16:16:42 +0100

    Prova

--- IPMeteoLocation/src/main/Main.java
+++ IPMeteoLocation/src/main/Main.java
@@ -7,15 +7,17 @@ import javax.xml.bind.JAXBException;
 import javax.xml.bind.Unmarshaller;
 
 import model.Weather;
+import controller.ControllerIPWindow;
+import controller.ControllerMeteoWindow;
+import view.IPWindow;
 import view.MeteoWindow;
 
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		new MeteoWindow();
-		URL file = Weather.class.getResource("weatherData.xml");
-		Weather customer;
+		new ControllerMeteoWindow(new Weather(), new MeteoWindow());
+		/*URL file = Weather.class.getResource("http://api.openweathermap.org/data/2.5/forecast?mode=xml&lat=150&lon=15&cnt=10&appid=42b223d0b7441d97bd051375c1c1f0a1");
+		Weather customer = null;
 		try {
 			JAXBContext jaxbContext =JAXBContext.newInstance(Weather.class);
 			Unmarshaller jaxbUnmarshaller = jaxbContext.createUnmarshaller();
@@ -23,6 +25,9 @@ public class Main {
 		} catch (JAXBException e) {
 			e.printStackTrace();
 		}
+		System.out.println(customer.getForecast().getTime().getClouds().getValue());
+		IPWindow f=new IPWindow();
+		ControllerIPWindow c=new ControllerIPWindow(f);*/
 	}
 
 }
