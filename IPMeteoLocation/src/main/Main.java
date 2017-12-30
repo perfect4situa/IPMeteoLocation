266bd5b55e3d3512857470585842fce38d138337
perfect4situa <marcocolusso99@gmail.com>
2017-12-30 18:35:56 +0100

    start ip win

--- IPMeteoLocation/src/main/Main.java
+++ IPMeteoLocation/src/main/Main.java
@@ -1,18 +1,14 @@
 package main;
 
-import model.Previsione;
 import controller.ControllerIPWindow;
-import controller.ControllerMeteoWindow;
 import view.IPWindow;
-import view.MeteoWindow;
 
 public class Main {
 
 	public static void main(String[] args) {
 		
-		IPWindow f=new IPWindow();
-		//new ControllerMeteoWindow(new Previsione("treviso"), new MeteoWindow());
-		ControllerIPWindow c=new ControllerIPWindow(f);
+		new ControllerIPWindow(new IPWindow());
+		
 	}
 
 }
